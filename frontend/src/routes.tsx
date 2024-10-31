01f83c0c35eca328adcc87afbeacf05680487c32
duwan <duwandc969@gmail.com>
2024-10-31 03:54:28 -0800

    'completed'

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,18 +1,32 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import BasicLayout from "./layout/BasicLayout";
 import NotFoundPage from "./pages/error/NotFound";
-import MainPage from "./pages/index";
+import Upload from "./pages/upload/UploadPage";
+import MrfListPage from "./pages/mrflist/MrfList";
 
 const router = createBrowserRouter([
   {
+    path: "/", // Add path here for BasicLayout
     element: <BasicLayout />,
+    errorElement: <NotFoundPage />,
     children: [
       {
-        path: "/",
-        element: <MainPage />,
+        path: "/", // Use index to define the default child route
+        element: <Navigate to={"/upload"} />,
       },
+      {
+        path: "upload", // Move the signin route under the layout
+        element: <Upload />,
+      },
+      {
+        path: "rmffiles", // Move the signin route under the layout
+        element: <MrfListPage />,
+      }
     ],
-    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "*", // Catch-all for undefined routes
+    element: <NotFoundPage />,
   },
 ]);
 
